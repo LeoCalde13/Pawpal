674aeff7f7767eb6d69b60f07b1ab4d3cc67d487
LeoCalde13 <leocalde13@hotmail.com>
2025-04-11 21:23:22 +0100

    + added dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.google.ksp)
 }
 
 android {
@@ -56,4 +57,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation(libs.moshi.kotlin)
+    implementation(libs.glide)
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.squareup.okhttp3)
+    ksp(libs.androidx.room.compiler)
 }
